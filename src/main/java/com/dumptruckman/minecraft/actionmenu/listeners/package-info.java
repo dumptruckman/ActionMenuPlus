ce5762351789cbbe810cdc698c70679672940f4b
Jeremy Wood <farachan@gmail.com>
2013-04-18 16:23:10 -0400

    package-info.javas

---
+++ src/main/java/com/dumptruckman/minecraft/actionmenu/listeners/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Contains many pre-made listeners for a variety of ways of detecting a player's interaction with a menu.
+ */
+package com.dumptruckman.minecraft.actionmenu.listeners;
--- src/main/java/com/dumptruckman/minecraft/actionmenu/package-info.java
+++ src/main/java/com/dumptruckman/minecraft/actionmenu/package-info.java
@@ -2,5 +2,7 @@
  * Contains the primary API classes for ActionMenuPlus.
  * <p/>
  * See {@link Menu} for creating a menu and {@link MenuView} for creating a view to display the menu.
+ * <p/>
+ * For extra simple menu creation, check out the {@link com.dumptruckman.minecraft.actionmenu.prefab.Menus} class.
  */
 package com.dumptruckman.minecraft.actionmenu;
